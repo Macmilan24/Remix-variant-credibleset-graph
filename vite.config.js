350728c6f4f3bf889152fbdfa17e19200aa173ce
Macmilan24 <cpt.macmilan24@gmail.com>
2025-07-28 13:08:35 +0300

    fix: add installGlobals call and update serverDependenciesToBundle in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { vitePlugin as remix } from "@remix-run/dev";
+import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import path from "path";
+
+installGlobals();
 
 export default defineConfig({
   plugins: [
@@ -12,6 +15,30 @@ export default defineConfig({
         v3_singleFetch: true,
         v3_lazyRouteDiscovery: true,
       },
+      serverDependenciesToBundle: [
+        "@material-ui/core",
+        "@material-ui/icons",
+        "d3",
+        "d3-array",
+        "d3-axis",
+        "d3-brush",
+        "d3-color",
+        "d3-dispatch",
+        "d3-drag",
+        "d3-ease",
+        "d3-format",
+        "d3-interpolate",
+        "d3-scale",
+        "d3-selection",
+        "d3-shape",
+        "d3-time",
+        "d3-time-format",
+        "d3-timer",
+        "d3-transition",
+        "d3-zoom",
+        "polished",
+        "react-measure",
+      ],
     }),
   ],
   resolve: {
