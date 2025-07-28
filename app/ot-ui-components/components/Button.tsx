d7a3619372a25897f5112f655ff96644b9e9d2c0
Macmilan24 <cpt.macmilan24@gmail.com>
2025-07-28 13:14:31 +0300

    fix: update import statement for MuiButton to include index.js

--- app/ot-ui-components/components/Button.tsx
+++ app/ot-ui-components/components/Button.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import { ButtonProps, default as MuiButton } from '@material-ui/core/Button';
+import React from "react";
+import {
+  ButtonProps,
+  default as MuiButton,
+} from "@material-ui/core/Button/index.js";
 
 const Button = ({ children, color, variant, ...rest }: ButtonProps) => (
   <MuiButton
-    color={color ? color : 'primary'}
-    variant={variant ? variant : 'contained'}
+    color={color ? color : "primary"}
+    variant={variant ? variant : "contained"}
     {...rest}
   >
     {children}
